79b29f7724d301f817b8a45e0fea927849489d09
lucas pancotto <lucas97pancotto@gmail.com>
2023-07-29 18:21:11 -0300

    data.py finished

--- 01-Project-Setup/data-context-and-setup/olist/data.py
+++ 01-Project-Setup/data-context-and-setup/olist/data.py
@@ -9,24 +9,28 @@ class Olist:
         Its keys should be 'sellers', 'orders', 'order_items' etc...
         Its values should be pandas.DataFrames loaded from csv files
         """
-        root_dir = os.path.dirname(os.path.dirname(__file__))
-        print('the root dir is ' ,root_dir)
-        csv_path = os.path.join(root_dir, 'data', 'csv')
-        print('looking in: ' , csv_path)
-        file_names = os.listdir(csv_path)[1:]
-        key_names = [csv.strip('dataset.csv').strip('olist').strip('\_') for csv in file_names]
-        print('file and key names done')
-        data = {}
-        for (key , file) in zip(key_names , file_names):
-            data[key] = pd.read_csv(os.path.join(csv_path, f'{file}'))
-        return data
-
         # Hints 1: Build csv_path as "absolute path" in order to call this method from anywhere.
             # Do not hardcode your path as it only works on your machine ('Users/username/code...')
             # Use __file__ instead as an absolute path anchor independant of your usename
             # Make extensive use of `breakpoint()` to investigate what `__file__` variable is really
         # Hint 2: Use os.path library to construct path independent of Mac vs. Unix vs. Windows specificities
-        pass  # YOUR CODE HERE
+        # $CHALLENGIFY_BEGIN
+        root_dir = os.path.dirname(os.path.dirname(__file__))
+        csv_path = os.path.join(root_dir, "data", "csv")
+
+        file_names = [f for f in os.listdir(csv_path) if f.endswith(".csv")]
+
+        key_names = [
+            key_name.replace("olist_", "").replace("_dataset", "").replace(".csv", "")
+            for key_name in file_names
+        ]
+
+        # Create the dictionary
+        data = {}
+        for k, f in zip(key_names, file_names):
+            data[k] = pd.read_csv(os.path.join(csv_path, f))
+        return data
+        # $CHALLENGIFY_END
 
     def ping(self):
         """
